989420a6980fa20f933ac048a62596f639dccd20
Julian Luna <juli.luna1999@gmail.com>
2015-10-28 09:51:08 -0300

    se cambian las torretas por naves

--- jular-Luna.py
+++ jular-Luna.py
@@ -4,11 +4,10 @@ import pilasengine
 pilas = pilasengine.iniciar()
 fondo=pilas.fondos.Fondo("fondo.jpg")
 fondo.definir_escala(2.5)
-jugador1=pilas.actores.Torreta()
+jugador1=pilas.actores.Nave()
 jugador1.x=-300
-jugador2=pilas.actores.Torreta()
+jugador2=pilas.actores.Nave()
 jugador2.x=300
-jugador1.aprender(pilas.habilidades.MoverseConElTeclado)
-
 jugador1.aprender(pilas.habilidades.LimitadoABordesDePantalla)
+jugador2.aprender(pilas.habilidades.LimitadoABordesDePantalla)
 pilas.ejecutar()
