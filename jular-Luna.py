b3508b8a44abbbf9f1fca0f7fab325aa1a46a27b
Julian Luna <juli.luna1999@gmail.com>
2015-10-28 13:53:31 -0300

    se agregaron los personajes

--- jular-Luna.py
+++ jular-Luna.py
@@ -4,14 +4,26 @@ import pilasengine
 pilas = pilasengine.iniciar()
 fondo=pilas.fondos.Fondo("fondo.jpg")
 fondo.definir_escala(2.5)
-class Alien(pilasengine.actores.Actor):
+
+
+class Tanque1(pilasengine.actores.Actor):
     def iniciar(self):
-        self.imagen = "alien.png"
+        self.imagen = "/home/julianluna/Grupo10/tanque1.png"
+
+class Tanque2(pilasengine.actores.Actor):
+    def iniciar(self):
+        self.imagen = "/home/julianluna/Grupo10/tanque2.png"
         
-jugador1=pilasengine.actores.Nave(x=-300, y=0)
-jugador1.x=-300
-jugador2=pilas.actores.Nave(x=300, y=0)
-jugador2.x=300
-jugador1.aprender(pilas.habilidades.LimitadoABordesDePantalla)
-jugador2.aprender(pilas.habilidades.LimitadoABordesDePantalla)
+jugador1 = Tanque1(pilas)
+jugador1.x =-300
+jugador1.definir_escala(0.3)
+
+
+jugador2 = Tanque2(pilas)
+jugador2.x = 300
+jugador2.definir_escala(0.3)
+jugador2.rotacion=180
+
+
+
 pilas.ejecutar()
