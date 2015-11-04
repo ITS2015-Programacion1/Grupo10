c38f8c8f876507132494b81340b6c97ea1772593
Julian Luna <juli.luna1999@gmail.com>
2015-11-04 11:43:55 -0300

    grilla de tanques

--- jular-Luna.py
+++ jular-Luna.py
@@ -5,11 +5,11 @@ fondo=pilas.fondos.Fondo("fondo.jpg")
 fondo.definir_escala(2.5)
 
 
-teclas = {pilas.simbolos.a: 'izquierda',
-                      pilas.simbolos.d: 'derecha',
-                      pilas.simbolos.w: 'arriba',
-                      pilas.simbolos.s: 'abajo',
-                      pilas.simbolos.ESPACIO: 'boton'}
+#teclas = {pilas.simbolos.a: 'izquierda',
+#                      pilas.simbolos.d: 'derecha',
+#                      pilas.simbolos.w: 'arriba',
+#                      pilas.simbolos.s: 'abajo',
+#                      pilas.simbolos.ESPACIO: 'boton'}
 
 class Tanque1(pilasengine.actores.Actor):
     def iniciar(self):
@@ -19,14 +19,14 @@ class Tanque2(pilasengine.actores.Actor):
 
     def iniciar(self):
         self.imagen = "/home/julianluna/Grupo10/tanque2.png"
-        mi_control = pilas.control.Control(pilas.escena_actual(), teclas)
-   def actualizar(self):
-        if pilas.escena_actual().control.izquierda:
-                self.x -= 5
-                self.espejado = True
-        elif pilas.escena_actual().control.derecha:
-            self.x += 5
-            self.espejado = False        
+        #mi_control = pilas.control.Control(pilas.escena_actual(), teclas)
+   #def actualizar(self):
+   #     if pilas.escena_actual().control.izquierda:
+    #            self.x -= 5
+         #       self.espejado = True
+   #     elif pilas.escena_actual().control.derecha:
+    #        self.x += 5
+   #         self.espejado = False        
 
 jugador1 = Tanque1(pilas)
 jugador1.x =-100
